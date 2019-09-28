074276c857cc738f1200e03f216ef66a55a927ce
season101 <sijan.malla101@gmail.com>
2019-09-28 14:10:28 -0500

    Almost getting started with Steganography library

--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-print("Hello World")
+print("Welcome to the program where we will encrpypt your passowrd in image for you.")
+
+print()
+
+print("Press any key to continue...")
+input()
+
+print()
+print()
+
+masterKey = ("Enter a key that you want to use throughout your lifetime: ")
+imageName = input("Enter the image name you want to put password to: ")
+outputName = input("Enter the name for your output image: ")
+
+
+imageName+= ".jpg"
+
+
+from cryptosteganography import CryptoSteganography
+
+crypto_steganography = CryptoSteganography(masterKey)
+'''
+# Save the encrypted file inside the image
+crypto_steganography.hide(imagename, 'Encrypted1.png', 'We will rock on')
+
+print("Successfully Encrypted")
+
+'''
+secret = crypto_steganography.retrieve('Encrypted1.png')
+
+print(secret)
+# My secret message
